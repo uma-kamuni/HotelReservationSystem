475db9dcf22d2240a83787f75b2c4152b587ba98
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-12 08:53:07 -0800

    Welcome to Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationSystem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Hotel Reservation Program");
+	}
+
+}
